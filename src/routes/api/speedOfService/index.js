ad7b6b835b1aa984f454416c130605679c1dc781
Nelson Segarra <junicus@outlook.com>
2018-04-19 15:13:17 -0400

    return fake data

--- src/routes/api/speedOfService/index.js
+++ src/routes/api/speedOfService/index.js
@@ -18,7 +18,12 @@ routes.get('/summaries', passport.authenticate('oauth-bearer', { session: false 
   const reqDate = getDateFromQuery(date);
   res.json({
     success: true,
-    data: []
+    data: {
+      keys: { 0: '0 to 5', 1: '5 to 7', 2: '7 to 10', 3: '10 to 15' },
+      records: [
+        { date, daypart: 'Lunch', summaries: { 0: 59, 1: 60, 2: 40, 3: 30 } }
+      ]
+    }
   });
 });
 
@@ -31,7 +36,10 @@ routes.get('/averages', passport.authenticate('oauth-bearer', { session: false }
   const reqDate = getDateFromQuery(date);
   res.json({
     success: true,
-    data: []
+    data: [
+      { date, daypart: 'Lunch', average: 8.9 },
+      { date, daypart: 'Dinner', average: 10.32 }
+    ]
   });
 });
 
